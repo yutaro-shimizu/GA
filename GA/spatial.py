d9a51bd795096ac275f2606d27a1c93b9589e865
yutaro-shimizu <68834841+yutaro-shimizu@users.noreply.github.com>
2022-07-24 18:31:40 -0600

    fix bugs for deepcopy

--- GA/spatial.py
+++ GA/spatial.py
@@ -1,5 +1,6 @@
 #import neural network pacakages
 from sklearn.neural_network import MLPClassifier
+from copy import deepcopy
 
 import numpy as np
 import random 
@@ -14,20 +15,20 @@ def load_data(train_csv='mnist_train.csv',test_csv='mnist_test.csv'):
     # load data
     print("load data")
 
-    data_train = pd.read_csv(train_csv) #load MNIST training data in
-    data_train = np.array(data_train) #turn into array
-    m, n =data_train.shape
+    data_train = pd.read_csv(train_csv) # load MNIST training data in
+    data_train = np.array(data_train)   # turn into array
+    m, n = data_train.shape
     np.random.shuffle(data_train)
-    Y_train=data_train[:,0]
-    X_train=data_train[:,1:n]
+    Y_train = data_train[:, 0]
+    X_train = data_train[:, 1:n]
 
-    data_test = pd.read_csv(test_csv) #validating data loaded in
-    data_test = np.array(data_test) #turned to array and transposed
+    data_test = pd.read_csv(test_csv)   # validating data loaded in
+    data_test = np.array(data_test)     # turned to array and transposed
     p, q = data_test.shape
     np.random.shuffle(data_test)
 
-    y_test =data_test[:,0] #first row of data
-    X_test = data_test[:,1:q] #rest of data
+    y_test = data_test[:, 0]    # first row of data
+    X_test = data_test[:, 1:q]  # rest of data
 
     #next two lines are taking 10,000 samples from MNIST
     X_train, X_val = X_train[:10000], X_train[10000:20000]
@@ -36,26 +37,32 @@ def load_data(train_csv='mnist_train.csv',test_csv='mnist_test.csv'):
     print("load data complete")
     return X_train, X_val, X_test, y_train, y_val, y_test
 
-class Initializer:
+class Spatial_GA:
     def __init__(self, hid_nodes):
-        self.NNs = {} #  set of models for evolution. Swaps based on training score during evolution. 
-        self.NNs_copy = {} # for reference during evolution. Does not change during swaps.
+        self.NNs = {}  # set of models for evolution. Swaps based on training score during evolution. 
+        self.NNs_copy = {}  # for reference during evolution. Does not change during swaps.
         self.all_train_score = []
         self.all_val_score = []
         self.neighbors = []
 
     def birth(self, population, hid_nodes, X_train, y_train):
+        """
+        Produce population each individual containing model, training score and validation score attributes.
+        """
         for ind in range(population):
-            self.NNs[ind] = {"model":MLPClassifier(hidden_layer_sizes=(hid_nodes,), max_iter=1, alpha=1e-4,
-                          solver='sgd', verbose=10, learning_rate_init=.1),
-                        "train_score":0,
-                        "val_score":0}
+            self.NNs[ind] = {"model": MLPClassifier(hidden_layer_sizes=(hid_nodes,), max_iter=1, alpha=1e-4,
+                          solver='sgd', verbose=False, learning_rate_init=.1),
+                        "train_score": 0,
+                        "val_score": 0}
             self.NNs[ind]["model"].fit(X_train, y_train) # fit the network to initialize W and b
             # randomly initialize weights and biases
-            self.NNs[ind]["model"].coefs_[0] = np.random.uniform(low=-1,high=1,size=(784,hid_nodes)) 
-            self.NNs[ind]["model"].coefs_[1] = np.random.uniform(low=-1,high=1,size=(hid_nodes,10))
+            self.NNs[ind]["model"].coefs_[0] = np.random.uniform(low=-1, high=1, size=(784, hid_nodes)) 
+            self.NNs[ind]["model"].coefs_[1] = np.random.uniform(low=-1, high=1, size=(hid_nodes, 10))
 
     def calculator(self, X_train, y_train, X_val, y_val):
+        """
+        Calculate max score for each generation. Store max score in the array.
+        """
         train_score = []
         val_score = []
         for ind in self.NNs:
@@ -63,49 +70,60 @@ class Initializer:
             self.NNs[ind]["val_score"]= self.NNs[ind]["model"].score(X_val, y_val)
             train_score.append(self.NNs[ind]["train_score"])
             val_score.append(self.NNs[ind]["val_score"])
-        self.NNs_copy = self.NNs # copy original including the scores
+        self.NNs_copy = deepcopy(self.NNs) # copy original including the scores
         print("Max training score: ", np.amax(train_score))
         print("Max validation score: ", np.amax(val_score))
         self.all_train_score.append(np.amax(train_score))
         self.all_val_score.append(np.amax(val_score))
 
-    def identify_max_neighbor(self,i,j,dim,neigh_size): #fix the modulus equation
-        #take the score and compare
-        score = self.NNs_copy[i*dim+j]["train_score"]
-        idx = i*dim+j
-        llim = -int(neigh_size/2)
-        rlim = int(neigh_size/2)
+    def identify_max_neighbor(self, i, j, dim, neigh_size): 
+        score = self.NNs_copy[i * dim + j]["train_score"]
+        idx = i * dim + j
+        llim = - int(neigh_size / 2)
+        rlim = int(neigh_size / 2)
+
+        """
+        ################### add probabilistic replacement###########
+        """
         for k in range(llim,rlim+1):
             for l in range(llim,rlim+1):
-                if score < self.NNs_copy[((i+k)%dim)*dim+(j+l)%dim]["train_score"]:
-                    score = self.NNs_copy[((i+k)%dim)*dim+(j+l)%dim]["train_score"]
-                    idx = ((i+k)%dim)*dim+(j+l)%dim
-                    print("swapped")
+                if score < self.NNs_copy[((i + k) % dim) * dim + (j + l) % dim]["train_score"]:
+                    score = self.NNs_copy[((i + k) % dim) * dim + (j + l) % dim]["train_score"]
+                    idx = ((i + k) % dim) * dim + (j + l) % dim
         return idx
 
     def mutate(self, idx, mut_rate):
+        """
+        In each layer, mutate weights at random cites with probability "mut_rate"
+        ---
+        idx: int
+
+        """
         for i in range(2):
             coef_size = self.NNs[idx]["model"].coefs_[i].size
             shape = self.NNs[idx]["model"].coefs_[i].shape
-            mutate_idx = np.random.choice(coef_size,size = int(mut_rate*coef_size))
+            mutate_idx = np.random.choice(coef_size, size=int(mut_rate * coef_size))
             for loci in mutate_idx:
                 self.NNs[idx]["model"].coefs_[i].flat[loci] += np.random.normal(loc=0.05)
             self.NNs[idx]["model"].coefs_[i].reshape(shape)
         return None
     
-    def replace_neighbors(self,dim,neigh_size,mut_rate=0.05):
-        for i in range(dim):
-            for j in range(dim):
-                idx = i*dim+j
-                self.NNs[idx] = self.NNs_copy[self.identify_max_neighbor(i,j,dim,neigh_size)]
+    def probe_neighbors(self,dim,neigh_size,mut_rate=0.05):
+        """
+        - Check every cell and apply probabilistic replacement and mutation
+        - Mutation happens for each layer
+        """
+        for i in range(dim): # for every row
+            for j in range(dim): #for every column
+                idx = i*dim+j # convert row and column notations to an index 
+                self.NNs[idx] = deepcopy(self.NNs_copy[self.identify_max_neighbor(i,j,dim,neigh_size)])
                 self.mutate(idx, mut_rate)
-        self.NNs_copy = self.NNs
 
 def run():
 
     ######### 1.Set Hyperparameters #########
-    generations = 10 #int(sys.argv[1]) #10
-    dimension = 4 #int(sys.argv[2]) #10
+    generations = int(sys.argv[1]) #10
+    dimension = int(sys.argv[2]) #10
     population = dimension ** 2
     hid_nodes = 10 #int(sys.argv[3]) #10
     mut_rate = 0.05 #float(sys.argv[4]) #0.05
@@ -115,18 +133,18 @@ def run():
     X_train, X_val, X_test, y_train, y_val, y_test = load_data()
 
     ######### 3.Initialize Populatioin #########
-    initializer = Initializer(hid_nodes)
-    initializer.birth(population,hid_nodes,X_train,y_train)
+    spaceGA = Spatial_GA(hid_nodes)
+    spaceGA.birth(population,hid_nodes,X_train,y_train)
 
     ######### 4.Run Spatial Evolution #########
     for i in range(generations):
-        initializer.calculator(X_train, y_train, X_val, y_val)
-        initializer.replace_neighbors(dimension,neighbor_size,mut_rate)
-        print(initializer.NNs)
+        print("\ncurrent generation: ", i)
+        spaceGA.calculator(X_train, y_train, X_val, y_val)
+        spaceGA.probe_neighbors(dimension,neighbor_size,mut_rate)
 
     ######### 5.Plot Result #########
-    plt.plot(initializer.all_train_score, label = "training")
-    plt.plot(initializer.all_val_score, label = "validation")
+    plt.plot(spaceGA.all_train_score, label = "training")
+    plt.plot(spaceGA.all_val_score, label = "validation")
     plt.legend()
     plt.show(block=True)
 
